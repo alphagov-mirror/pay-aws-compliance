520ba0c7230f94a3b5b470c83f4dc54945f2a1aa
Dominic Jones <dominic.jones@digital.cabinet-office.gov.uk>
2021-02-11 12:29:58 +0000

    BAU Point to reboots required guidance
    To avoid confusion about what to do with this alert

--- aws_compliance.py
+++ aws_compliance.py
@@ -222,7 +222,7 @@ def reboots_required():
     failReason = ""
     offenders = []
     control = "reboots_required"
-    description = "Instances requiring a reboot, see /var/log/pay-reboots-required.log on instances"
+    description = "Instances requiring a reboot, see /var/log/pay-reboots-required.log on instances. See guidance https://pay-team-manual.cloudapps.digital/manual/suppor"
     scored = False
     filters = [{'Name':'tag:reboots_required', 'Values':['true']}]
     reservations = EC2_CLIENT.describe_instances(Filters=filters).get('Reservations', [])
